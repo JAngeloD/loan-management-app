da5158fb776e5ad5be2628f4b355ef77e430a953
Angelo Devera <deveraangelo319@gmail.com>
2023-03-06 20:59:36 -0700

    Finalized field content and styling

--- src/components/borrowerform/BorrowerForm.js
+++ src/components/borrowerform/BorrowerForm.js
@@ -8,39 +8,36 @@ function BorrowerForm() {
           <div class="card-header bg-dark-subtle">
             <h5 class="mb-1">Add Borrower</h5>
           </div>
-          <div class="p-3">
-            <form id="survey-form">
-
+          <div class="p-3 fs-5 fw-semibold">
+            <form class="">
               <div class="row">
-
                 <div class="col-md-6">
                   <div class="form-group">
                     <label id="name-label" for="name">Name</label>
-                    <input type="text" name="name" id="name" placeholder="Enter borrower's name" class="form-control" required />
+                    <input type="text" name="name" id="name" placeholder="Enter borrower's name" class="form-control bg-secondary-subtle mt-1 p-3" required />
                   </div>
                 </div>
 
                 <div class="col-md-6">
                   <div class="form-group">
-                    <label id="email-label" for="email">Email</label>
-                    <input type="email" name="email" id="email" placeholder="Enter your email" class="form-control" required />
+                    <label for="startdate">Starting date</label>
+                    <input type="date" name="startdate" id="startdate" class="form-control bg-secondary-subtle mt-1 p-3"  required />
                   </div>
                 </div>
-
               </div>
 
               <div class="row">
-                <div class="col-md-6">
+              <div class="col-md-6">
                   <div class="form-group">
-                    <label id="name-label" for="name">Name</label>
-                    <input type="text" name="name" id="name" placeholder="Enter borrower's name" class="form-control" required />
+                    <label for="loanamount">Loan Amount</label>
+                    <input type="number" name="loanamount" id="loanamount" min="1" class="form-control bg-secondary-subtle mt-1 p-3" placeholder="$0.00" />
                   </div>
                 </div>
 
                 <div class="col-md-6">
                   <div class="form-group">
-                    <label id="email-label" for="email">Email</label>
-                    <input type="email" name="email" id="email" placeholder="Enter your email" class="form-control" required />
+                    <label for="term">Term</label>
+                    <input type="number" name="term" id="term" min="1" class="form-control bg-secondary-subtle mt-1 p-3" placeholder="0"/>
                   </div>
                 </div>
               </div>
@@ -49,7 +46,7 @@ function BorrowerForm() {
                 <div class="col-md-6">
                   <div class="form-group">
                     <label>Frequency</label>
-                    <select id="dropdown" name="role" class="form-control" required>
+                    <select id="dropdown" name="frequency" class="form-control bg-secondary-subtle mt-1 p-3" required>
                       <option disabled selected value>Select</option>
                       <option value="monthly">Monthly</option>
                       <option value="biweekly">Bi-Weekly</option>
@@ -62,8 +59,8 @@ function BorrowerForm() {
 
                 <div class="col-md-6">
                   <div class="form-group">
-                    <label id="number-label" for="number">Interest <small>(optional)</small></label>
-                    <input type="number" name="age" id="number" min="10" max="99" class="form-control" placeholder="Age" />
+                    <label for="interest">Interest</label>
+                    <input type="number" name="interest" id="interest" min="1" max="100" class="form-control bg-secondary-subtle mt-1 p-3 "  placeholder="#%" />
                   </div>
                 </div>
               </div>
@@ -72,17 +69,16 @@ function BorrowerForm() {
                 <div class="col-md-12 mt-2">
                   <div class="form-group">
                     <label>Remarks</label>
-                    <textarea id="remark" class="form-control" name="remark" placeholder="Enter any remarks for this borrower..."></textarea>
+                    <textarea id="remark" class="form-control bg-secondary-subtle mt-1 p-3" name="remark" placeholder="Enter any remarks for this borrower..." style={{height: "200px"}}></textarea>
                   </div>
                 </div>
               </div>
 
               <div class="row">
                 <div class="col-md-4 mt-4">
-                  <button type="submit" id="submit" class="btn btn-primary btn-block">Submit Survey</button>
+                  <button type="submit" id="submit" class="btn btn-primary btn-block">Add</button>
                 </div>
               </div>
-
             </form>
           </div>
         </div>

06798d662fc390b1c46bd4d8cd2ce08f457d0ffa
Angelo Devera <deveraangelo319@gmail.com>
2023-03-08 20:23:16 -0700

    Added state hook & handling to switch between screens

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import BorrowerForm from './borrowerform/BorrowerForm'
 import Dashboard from './dashboard/Dashboard'
 import Navbar from './Navbar/Navbar'
 
 function App() {
+
+  //State of the main screen, dashboard is the default
+  const [mainState, setMainState] = useState("dashboard")
+  const handleState = (newState) => {setMainState(newState)}
+
   return (
     <>
       <div class="container-fluid">
         <div class="row flex-nowrap">
-          <Navbar></Navbar>
-          <BorrowerForm></BorrowerForm>
+          <Navbar handleState={handleState}/>
+
+          {(() => {
+            switch (mainState) {
+              case "dashboard":
+                return <Dashboard/>
+              case "borrowerform":
+                return <BorrowerForm/>
+              default:
+                return <Dashboard/>
+            }
+          })()}
+
         </div>
       </div>
     </>
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Navbar() {
+function Navbar(props) {
   return (
     <>
       <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark shadow-lg">
@@ -9,29 +9,33 @@ function Navbar() {
             <span class="fs-5 d-none d-sm-inline">Loan App</span>
           </a>
           <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-            <li onClick={() => {
 
-            }}>
+            <li onClick={() => props.handleState("dashboard")}>
               <a href="#" class="nav-link align-middle px-0">
                 <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline ">Dashboard</span>
               </a>
             </li>
-            <li>
+
+            <li onClick={() => props.handleState("borrowerform")}>
               <a href="#" class="nav-link px-0 align-middle">
                 <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Add Borrower</span>
               </a>
             </li>
-            <li>
+
+            <li onClick={() => props.handleState("archive")}>
               <a href="#" class="nav-link px-0 align-middle">
                 <i class="fs-4 bi-book"></i> <span class="ms-1 d-none d-sm-inline">Archive</span>
               </a>
             </li>
+
             <hr class="hr" style={{ width: "100%" }} />
-            <li>
+
+            <li onClick={() => props.handleState("settings")}>
               <a href="#" class="nav-link px-0 align-middle">
                 <i class="fs-4 bi bi-gear"></i> <span class="ms-1 d-none d-sm-inline">Settings</span>
               </a>
             </li>
+
           </ul>
         </div>
       </div>
